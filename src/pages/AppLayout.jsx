a54c938e4cb8513930ae93dbdf8c684664cead6b
Manik Maity <manikmaity156@gmail.com>
2024-08-21 13:22:32 +0530

    Header component made

--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,13 +1,18 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Header from "../components/Header/Header";
 
 function AppLayout() {
   return (
-    // Header 
-    <main>
-        <Outlet/>
-    </main>
-  )
+    <div className="bg-slate-900 text-white min-h-screen">
+      <div className="container marker px-8 py-6 mx-auto">
+        <Header/>
+        <main>
+          <Outlet />
+        </main>
+      </div>
+    </div>
+  );
 }
 
-export default AppLayout
+export default AppLayout;
