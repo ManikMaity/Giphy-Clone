a247df88b9f3f77d3b4b711873079d0b969944e5
Manik Maity <manikmaity156@gmail.com>
2024-08-22 10:22:28 +0530

    Styled header and made search box element

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,11 +2,13 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import logo from "../../assets/logo.svg";
 import { HiDotsVertical } from "react-icons/hi";
+import { FiAlignRight } from "react-icons/fi";
+import SearchBox from "./SearchBox";
 
 function Header() {
   const [categories, setCategories] = useState();
   return (
-    <nav>
+    <nav className="flex flex-col justify-center relative">
       <div className="flex items-center gap-4 h-10 justify-between mb-2">
         <Link className="flex gap-2 items-center" to="/">
           <img src={logo} className="w-8" alt="logo" />
@@ -16,7 +18,7 @@ function Header() {
         {/* Rendering categories here  */}
         <Link
           to="/reaction"
-          className="px-4 py-1 hover:gradient transition-all hidden md:block border-b-4"
+          className=" hover:gradient transition-all h-full hidden md:grid place-content-center px-4 border-b-4"
         >
           Reaction
         </Link>
@@ -25,7 +27,26 @@ function Header() {
         <button onClick={() => setCategories(!categories)} className={`h-full w-8 ${categories ? "gradient" : ""} hover:gradient transition-all hidden md:block border-b-4`}>
           <HiDotsVertical className="h-full mx-auto" size={20} />
         </button>
+
+        <div className="h-full bg-gray-700 grid place-content-center px-6 cursor-pointer rounded">
+            <Link to="/favorites">Favorite GIF</Link>
+        </div>
+
+
+        <button className="block md:hidden">
+          <FiAlignRight size={25} className="text-sky-400 "/>
+        </button>
       </div>
+
+      {categories && <div className="absolute right-0 top-14 px-10 py-6 rounded w-full gradient z-20">
+        <span>Categories</span>
+        <hr />
+        <div>
+          <Link >Reactions</Link>
+        </div>
+        </div>}
+
+        <SearchBox/>
     </nav>
   );
 }
--- src/components/Header/SearchBox.jsx
+++ src/components/Header/SearchBox.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import { IoSearch } from 'react-icons/io5'
 
-function SearchBox() {
+function SearchBox({onclickfn}) {
+
+  function onSubmit (e){
+    e.preventDefault();
+    onclickfn?.();
+  }
+
   return (
-    <div>
-      
-    </div>
+    <form className='text-2xl w-full mt-2 flex rounded overflow-hidden '>
+      <input type="text" name="" id="" className='w-[92%] px-4 text-gray-950 py-5 outline-none border-none ' placeholder='Search gifs and skickers' />
+      <button onClick={onSubmit} type="submit" className='w-[8%] text-4xl grid place-content-center gradient outline-none border-none '><IoSearch /></button>
+    </form>
   )
 }
 
